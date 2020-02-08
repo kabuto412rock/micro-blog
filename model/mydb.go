789a98ceb8233f2ee3c7fd0e19d062abaff6a0e1
guzongjia <kabuto412rock@gmail.com>
2020-02-08 15:54:29 +0800

    建立.gitignore忽略debug的產物

---
+++ model/mydb.go
@@ -0,0 +1,67 @@
+package model
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	// Mysql的Driver
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+/*Article 資料表是MySQL資料庫UserDB中的資料表，
+儲存使用者撰寫的文章*/
+type Article struct {
+	ArticleID int
+	UserID    int
+	Title     string
+	Content   string
+	EditTime  time.Time
+}
+type MyDB struct {
+	db *sql.DB
+}
+
+/*New 產生一個DB實例*/
+func New() (MyDB, error) {
+	user := "dbuser"         // 帳號
+	password := "Ej3yj/ru8@" // 密碼
+	dbName := "UserDB"       // db名稱
+
+	// 連接本地的MySQL資料庫
+	db, err := connectMysql(user, password, dbName)
+	
+	return MyDB{db: db}, err
+}
+
+// Close 關閉MyDB內部的*sql.db實例
+func (mydb MyDB) Close() error {
+	return mydb.db.Close()
+}
+
+// 連接到本地端名為dbName的資料庫
+func connectMysql(userName, userPassword, dbName string) (*sql.DB, error) {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s", userName, userPassword, dbName)
+
+	return sql.Open("mysql", dataSourceName)
+}
+
+func (myDB MyDB) GetAllArticle() ([]Article, error) {
+	db := myDB.db
+	var results []Article
+	rows, err := db.Query("SELECT articleID, userID, title, content, editTime FROM Article", nil)
+	if err != nil {
+		return nil, err
+	}
+	if rows == nil {
+		return nil, err
+	}
+	var result Article
+	for rows.Next() {
+		rows.Scan(&result.ArticleID, &result.UserID, &result.Title, &result.Content, &result.EditTime)
+		fmt.Println("我進來rows.Next啦")
+		results = append(results, result)
+	}
+	return results, nil
+}
