7eb6ddd23e822a22db15b9560710302039ba4667
guzongjia <kabuto412rock@gmail.com>
2020-02-12 11:45:28 +0800

    將編輯文章＆刪除文章的區塊從articleList.html中分離, 刪除文章時會跳出dialog再確認一次！

---
+++ static/js/articleList.js
@@ -0,0 +1,49 @@
+var createPane = document.getElementById("createArticlePane")
+var showCreatePanBtn = document.getElementById("showCreatePaneButton")
+
+// 顯示id為'createArticlePane'的新建文章區塊(如果已經顯示，則隱藏。)
+function showCreatePane() {
+  if (createPane.hidden) {
+    showCreatePanBtn.innerText = "取消新文章"
+  } else {
+    showCreatePanBtn.innerText = "新文章"
+  }
+  createPane.hidden = !createPane.hidden
+}
+
+// 顯示id為'edit<panID>'的編輯區塊(如果已經顯示，則隱藏。)
+function showEditPane(paneID) {
+  var pane = document.getElementById('edit'+paneID)
+  var showEditButton = document.getElementById("showEditButton"+paneID)
+  if (pane.hidden == true) {
+      showEditButton.innerText = "取消編輯"
+  } else {
+      showEditButton.innerText = "編輯"
+  }
+  pane.hidden = !pane.hidden
+}
+
+// 直接發送一個刪除文章的POST請求，「刪除對應articleID的文章」
+function deleteArticle(articleID) {
+  var form = document.createElement('form');
+  form.setAttribute('method', 'post');
+  form.setAttribute('action', '/delete?articleID='+articleID);
+  form.style.display = 'hidden';
+  document.body.appendChild(form)
+  form.submit();
+}
+
+// 顯示'刪除確認的dialog'，「避免」誤點擊刪除按鈕導致「直接刪除」。
+function showDeleteDialog(articleID) {
+  var dialog = document.getElementById('deleteDialog'+articleID)
+  dialog.showModal();
+  dialog.addEventListener('click', function (event) {
+    var rect = dialog.getBoundingClientRect();
+    var isInDialog=(rect.top <= event.clientY && event.clientY <= rect.top + rect.height
+      && rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+    if (!isInDialog) {
+        dialog.close();
+    }
+});
+
+}
